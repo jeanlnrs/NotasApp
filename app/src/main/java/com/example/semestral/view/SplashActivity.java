08cfefbcd882e0a14376b94613b4cb1c693de4a0
Jean Linares <jean.lnrs@outlook.com>
2019-07-21 17:25:09 -0500

    Proyecto a entregar

---
+++ app/src/main/java/com/example/semestral/view/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.semestral.view;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.example.semestral.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 8000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
